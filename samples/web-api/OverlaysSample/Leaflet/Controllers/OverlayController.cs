894b67cf4c2d6b1873fee9115274d173d4bee2c3
jackli-tg <jackli@thinkgeo.com>
2020-04-10 10:28:09 -0500

    thinkgeo/public/thinkgeo-web-maps#1 Update OverlaysSample part 1

--- samples/web-api/OverlaysSample/Leaflet/Controllers/OverlayController.cs
+++ samples/web-api/OverlaysSample/Leaflet/Controllers/OverlayController.cs
@@ -1,34 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Web.Http;
-using Newtonsoft.Json;
-using ThinkGeo.MapSuite;
-using ThinkGeo.MapSuite.Drawing;
-using ThinkGeo.MapSuite.Layers;
-using ThinkGeo.MapSuite.Shapes;
-using ThinkGeo.MapSuite.Styles;
-using ThinkGeo.MapSuite.WebApi;
+using ThinkGeo.Core;
+using ThinkGeo.UI.WebApi;
 
 namespace ThinkGeo.MapSuite.Overlays
 {
-    [RoutePrefix("Overlays")]
-    public class OverlayController : ApiController
+    [ApiController]
+    [Route("Overlays")]
+    public class OverlayController : ControllerBase
     {
-        private static readonly string baseDirectory;
         private static Collection<Layer> customLayers;
 
         static OverlayController()
         {
-            baseDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data");
             InitializeCustomLayers();
         }
 
@@ -37,7 +28,7 @@ namespace ThinkGeo.MapSuite.Overlays
         /// </summary>
         [Route("LoadCustomOverlay/{z}/{x}/{y}")]
         [HttpGet]
-        public HttpResponseMessage LoadCustomOverlay(int z, int x, int y)
+        public IActionResult LoadCustomOverlay(int z, int x, int y)
         {
             LayerOverlay layerOverlay = new LayerOverlay();
             // Get custom overlay.
@@ -76,24 +67,19 @@ namespace ThinkGeo.MapSuite.Overlays
         /// <summary>
         /// Draws the map and return the image back to client in an HttpResponseMessage. 
         /// </summary>
-        private HttpResponseMessage DrawTileImage(LayerOverlay layerOverlay, int z, int x, int y)
+        private IActionResult DrawTileImage(LayerOverlay layerOverlay, int z, int x, int y)
         {
-            using (Bitmap bitmap = new Bitmap(256, 256))
+            using (GeoImage image = new GeoImage(256, 256))
             {
-                PlatformGeoCanvas geoCanvas = new PlatformGeoCanvas();
+                GeoCanvas geoCanvas = GeoCanvas.CreateDefaultGeoCanvas();
                 RectangleShape boundingBox = WebApiExtentHelper.GetBoundingBoxForXyz(x, y, z, GeographyUnit.Meter);
-                geoCanvas.BeginDrawing(bitmap, boundingBox, GeographyUnit.Meter);
+                geoCanvas.BeginDrawing(image, boundingBox, GeographyUnit.Meter);
                 layerOverlay.Draw(geoCanvas);
                 geoCanvas.EndDrawing();
 
-                MemoryStream ms = new MemoryStream();
-                bitmap.Save(ms, ImageFormat.Png);
-
-                HttpResponseMessage msg = new HttpResponseMessage(HttpStatusCode.OK);
-                msg.Content = new ByteArrayContent(ms.ToArray());
-                msg.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
-
-                return msg;
+                byte[] imageBytes = image.GetImageBytes(GeoImageFormat.Png);
+                  
+                return File(imageBytes, "image/png");
             }
         }
 
@@ -102,20 +88,15 @@ namespace ThinkGeo.MapSuite.Overlays
         /// </summary>
         private static void InitializeCustomLayers()
         {
-            Proj4Projection wgs84ToGoogleProjection = new Proj4Projection();
-            wgs84ToGoogleProjection.InternalProjectionParametersString = Proj4Projection.GetWgs84ParametersString(); //4326
-            wgs84ToGoogleProjection.ExternalProjectionParametersString = Proj4Projection.GetGoogleMapParametersString(); //900913
-            wgs84ToGoogleProjection.Open();
-
             customLayers = new Collection<Layer>();
-            string shpFilePathName = string.Format(@"{0}/POIs/Schools.shp", baseDirectory);
-            string schoolImage = string.Format(@"{0}/Images/school.png", baseDirectory);
+            string shpFilePathName = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "AppData\\POIs", "Schools.shp");
+            string schoolImage = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "AppData\\Images", "School.png");
             ShapeFileFeatureLayer schoolsLayer = new ShapeFileFeatureLayer(shpFilePathName);
             schoolsLayer.Name = "schoolLayer";
             schoolsLayer.Transparency = 200f;
             schoolsLayer.ZoomLevelSet.ZoomLevel10.DefaultPointStyle = new PointStyle(new GeoImage(schoolImage));
             schoolsLayer.ZoomLevelSet.ZoomLevel10.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
-            schoolsLayer.FeatureSource.Projection = wgs84ToGoogleProjection;
+            schoolsLayer.FeatureSource.ProjectionConverter = new ProjectionConverter(4326, 3857);
             customLayers.Add(schoolsLayer);
         }
     }

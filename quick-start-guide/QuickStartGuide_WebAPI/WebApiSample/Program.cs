178dad8db252fa57f8883ef028944a99bbe32139
Rui Li <ruili@thinkgeo.com>
2024-05-08 12:29:15 +0800

    thinkgeo/products/thinkgeosdk#688 Build QuickStartGuide_WebAPI_VS sample

---
+++ quick-start-guide/QuickStartGuide_WebAPI/WebApiSample/Program.cs
@@ -0,0 +1,36 @@
+using WebSampleNameSpace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Specify the URLs that the application will listen on
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+namespace WebSampleNameSpace
+{
+    public class WebSample 
+    {
+
+    }
+}

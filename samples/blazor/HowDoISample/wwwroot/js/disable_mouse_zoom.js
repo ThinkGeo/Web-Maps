3f2813124dd232df49c8b0145643849b72514930
Rui Li <ruili@thinkgeo.com>
2025-03-18 11:20:05 +0800

    thinkgeo/products/thinkgeosdk#798 Add Disable_Mouse_Zoom sample

---
+++ samples/blazor/HowDoISample/wwwroot/js/disable_mouse_zoom.js
@@ -0,0 +1,17 @@
+﻿window.DisableMouseWheel = function (mapId) {
+    // Retrieve the ThinkGeoMap from window.blazorObjects
+    let map = window.blazorObjects[mapId];
+    if (!map) {
+        console.warn("No map found for id: " + mapId);
+        return;
+    }
+
+    // Remove the MouseWheelZoom interaction
+    map.getInteractions().forEach(function (interaction) {
+        if (interaction instanceof ol.interaction.MouseWheelZoom) {
+            map.removeInteraction(interaction);
+        }
+    });
+
+    console.log("Mouse wheel zoom disabled for map:", mapId);
+};

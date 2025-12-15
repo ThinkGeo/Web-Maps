55b0521349b3e1d6b9497db7497c1952577726af
Ben Bai <benbai@thinkgeo.com>
2025-12-15 17:22:13 -0600

    thinkgeo/products/thinkgeosdk#896 Upgrade to .net8, and remove newtonsoft reference

--- samples/blazor/HowDoISample/Models/DemographicMapService.cs
+++ samples/blazor/HowDoISample/Models/DemographicMapService.cs
@@ -1,15 +1,20 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 
 namespace ThinkGeo.UI.Blazor.HowDoI
 {
     public class DemographicMapService
     {
+        private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
         public List<DemographicCategoryModel> GetDemographicCategories()
         {
             var menusFile = Path.Combine(Directory.GetCurrentDirectory(), "Data", "DemographicMap.json");
-            return JsonConvert.DeserializeObject<List<DemographicCategoryModel>>(File.ReadAllText(menusFile));
+            return JsonSerializer.Deserialize<List<DemographicCategoryModel>>(File.ReadAllText(menusFile), JsonOptions);
         }
     }
 }
